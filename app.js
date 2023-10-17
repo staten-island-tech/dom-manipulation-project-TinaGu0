9f3e1ee30265d79eb9cf74f83a1780798b8326ef
TinaGu0 <tinaguo0904@gmail.com>
2023-10-17 10:13:14 -0400

    heee heeeeeee

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 
 const DOMSelectors = {
-    background: document.querySelector("#bg"),
-    text: document.querySelector("#text"),
     form: document.querySelector("#form"),
     firstName:document.querySelector(".firstName"), 
-    h2s: document.querySelectorAll("h2"),
+    lastName:document.querySelector(".lastName"), 
+    DOB:document.querySelector(".DOB"),
+    school:document.querySelector(".school"),
+    grad:document.querySelector(".gradyr"),
 };
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault(); //when submit happens, don't refresh page
     console.log(DOMSelectors.firstName.value); //value is the valuet hat the user types in 
 });
-    
-function bg(background) {
-    background.style.backgroundColor = "#56725c";
-}
-bg(DOMSelectors.background);
-
-function txt(text) {
-    text.style.backgroundColor = "#e4dfec";
-    text.style.fontSize = "20px"
-}
-txt(DOMSelectors.text);

dfeafeb6724f80e0de0f083d1c8d325959f011d5
TinaGu0 <tinaguo0904@gmail.com>
2023-10-20 10:15:42 -0400

    eeeeeeeeeeeeeee

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 
-const DOMSelectors = {
+const DOMSelectors = { 
     form: document.querySelector("#form"),
     firstName:document.querySelector(".firstName"), 
     lastName:document.querySelector(".lastName"), 
     DOB:document.querySelector(".DOB"),
     school:document.querySelector(".school"),
     grad:document.querySelector(".gradyr"),
-};
+
+    
+}
+
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault(); //when submit happens, don't refresh page
 
 
     DOMSelectors.form.insertAdjacentHTML(
         "afterbegin", 
-        '<div class="card"><h2 class="card-header">${DOMSelectors.firstName.value}</h2></div>'
+        `<div class="card"><h2 class="card-header">${DOMSelectors.firstName.value}</h2></div>`
     );
 });
+
+

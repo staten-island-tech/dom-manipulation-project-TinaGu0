c115c2b83ec31416d1637adbb71a842c4f8406eb
TinaGu0 <tinaguo0904@gmail.com>
2023-10-31 10:13:47 -0400

    eeeeeeeee

--- app.js
+++ app.js
@@ -9,29 +9,26 @@ const DOMSelectors = {
     pic:document.querySelector(`#pic`),
 }
  
-let firstName = DOMSelectors.firstName.value
-let lastName = DOMSelectors.lastName.value
-let DOB = DOMSelectors.DOB.value
-let school= DOMSelectors.school.value
-let grad= DOMSelectors.grad.value
-let pic = DOMSelectors.pic.value
+
 
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault();
     DOMSelectors.form.insertAdjacentHTML("beforeend", 
-        `<div class="card">
+        `<div class="resultCard">
         <h1 class="header">${DOMSelectors.firstName.value} ${DOMSelectors.lastName.value}</h1>
         <h2 class="header">${DOMSelectors.school.value}</h2><img class="img" src="${DOMSelectors.pic.value}" />
         <h3>Date of Birth: ${DOMSelectors.DOB.value}</h3>
         <h3>Year of Graduation: ${DOMSelectors.grad.value}</h3>
-        <button class="button">REMOVE</button>
+        <button id="remove">REMOVE</button>
         </div>`
     );
+    remove()
 });
 
 function remove() {
-    let buttons =  document.querySelectorAll("button");
-    buttons.forEach((button)=> button.addEventListener("click" , function(event) {
+    let buttons =  document.querySelectorAll("remove");
+    buttons.forEach((remove)=> remove.addEventListener("click" , function(event) {
+        buttons.parentElement.remove(event);
     }));
  }
      

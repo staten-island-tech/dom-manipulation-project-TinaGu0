32573eacca89a23c610e3a46d029f7d03d75c0e0
TinaGu0 <tinaguo0904@gmail.com>
2023-10-30 22:32:56 -0400

    aaaaaaaaaaaa

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 
 const DOMSelectors = { 
     form: document.querySelector(`#form`),
-    firstName:document.querySelector(`#firstName`), 
-    lastName:document.querySelector(`#lastName`), 
-    DOB:document.querySelector(`#DOB`),
-    school:document.querySelector(`#school`),
-    grad:document.querySelector(`#grad`),
-    pic:document.querySelector(`#pic`),
+    firstName:document.getElementById("firstName"), 
+    lastName:document.getElementById("lastName"), 
+    DOB:document.getElementById("DOB"),
+    school:document.getElementById("school"),
+    grad:document.getElementById("grad"),
+    pic:document.getElementById("pic"),
 }
  
-let firstName = DOMSelectors.firstName.value
-let lastName = DOMSelectors.lastName.value
-let DOB = DOMSelectors.DOB.value
-let school= DOMSelectors.school.value
-let grad= DOMSelectors.grad.value
-let pic = DOMSelectors.pic.value
-
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault();
-    DOMSelectors.form.insertAdjacentHTML("beforeend", 
-        `<div class="card">
-        <h1 class="header">${DOMSelectors.firstName.value} ${DOMSelectors.lastName.value}</h1>
-        <h2 class="header">${DOMSelectors.school.value}</h2><img class="img" src="${DOMSelectors.pic.value}" />
-        <h3>Date of Birth: ${DOMSelectors.DOB.value}</h3>
-        <h3>Year of Graduation: ${DOMSelectors.grad.value}</h3>
-        <button class="button">REMOVE</button>
-        </div>`
-    );
+    makeCard();
+    `removeCard();`
+    clearValues()
 });
 
-function remove() {
-    let buttons =  document.querySelectorAll("button");
-    buttons.forEach((button)=> button.addEventListener("click" , function(event) {
-    }));
- }
-     
+function makeCard() {
+    DOMSelectors.form.insertAdjacentHTML("beforeend", 
+    `<div class="IDcard">
+    <h1 class="header">${DOMSelectors.firstName.value} ${DOMSelectors.lastName.value}</h1>
+    <h2 class="header">${DOMSelectors.school.value}</h2><img class="img" src="${DOMSelectors.pic.value}" />
+    <h3>Date of Birth: ${DOMSelectors.DOB.value}</h3>
+    <h3>Year of Graduation: ${DOMSelectors.grad.value}</h3>
+    <button id="clearButton">REMOVE</button>
+    </div>`
+)};
+
+`let button = document.querySelector("#clearButton")
+
+function removeCard() {
+    button.addEventListener("click", function(event) {
+        event.target.parentElement.remove()
+    })};
+`
+function clearValues() {
+    DOMSelectors.firstName.value=("")
+    DOMSelectors.lastName.value=("")
+    DOMSelectors.DOB.value=("")
+    DOMSelectors.school.value=("")
+    DOMSelectors.grad.value=("")
+    DOMSelectors.pic.value=("")
+};

3c97d18bad32dd31a3888f2e05b33c9f43fa6569
TinaGu0 <tinaguo0904@gmail.com>
2023-11-05 23:29:00 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa hi :3

--- app.js
+++ app.js
@@ -7,31 +7,49 @@ const DOMSelectors = {
     school:document.getElementById("school"),
     grad:document.getElementById("grad"),
     pic:document.getElementById("pic"),
-}
+    Cards:document.getElementById("Cards")
+};
  
-DOMSelectors.form.addEventListener("submit", function (event){
+DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     makeCard();
-    removeCard();
-    clearValues()
-});
+    clearValues();
+  });
 
 function makeCard() {
-    DOMSelectors.form.insertAdjacentHTML("beforeend", 
-    `<div class="IDcard">
-    <h1 class="header">${DOMSelectors.firstName.value} ${DOMSelectors.lastName.value}</h1>
-    <h2 class="header">${DOMSelectors.school.value}</h2><img class="img" src="${DOMSelectors.pic.value}" />
-    <h3>Date of Birth: ${DOMSelectors.DOB.value}</h3>
-    <h3>Year of Graduation: ${DOMSelectors.grad.value}</h3>
-    <button id="clearButton">REMOVE</button>
-    </div>`
-)};
+    let fn = DOMSelectors.firstName.value
+    let ln= DOMSelectors.lastName.value
+    let sch = DOMSelectors.school.value
+    let pc =DOMSelectors.pic.value
+    let bd = DOMSelectors.DOB.value
+    let gra = DOMSelectors.grad.value
+    DOMSelectors.Cards.insertAdjacentHTML("beforeend", 
+        `<div class="IDcard">
+        <h1 class="header">${fn} ${ln}</h1>
+        <h2 class="header">${sch}</h2>
+        <img class="img" src="${pc}" />
+        <h3>Date of Birth: ${bd}</h3>
+        <h3>Year of Graduation: ${gra}</h3>
+        <button class="clearButton">REMOVE</button>
+        </div>`);
+    removeCard();
+};
+
+  function removeCard(Cards) {
+    const removeButtons = Cards.querySelectorAll(".clearButton");
+    removeButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        DOMSelectors.Cards.remove();
+      });
+    });
+  };
+
 
 function clearValues() {
-    DOMSelectors.firstName.value=("")
-    DOMSelectors.lastName.value=("")
-    DOMSelectors.DOB.value=("")
-    DOMSelectors.school.value=("")
-    DOMSelectors.grad.value=("")
-    DOMSelectors.pic.value=("")
+    DOMSelectors.firstName.value=("");
+    DOMSelectors.lastName.value=("");
+    DOMSelectors.DOB.value=("");
+    DOMSelectors.school.value=("");
+    DOMSelectors.grad.value=("");
+    DOMSelectors.pic.value=("");
 };
